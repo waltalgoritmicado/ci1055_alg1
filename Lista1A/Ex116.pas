42cf1f8315f251b6b5c3ef5f8bb0b3dd49e7cf81
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-08 12:32:30 -0300

    Exercicio 116

--- Lista1A/Ex116.pas
+++ Lista1A/Ex116.pas
@@ -1,11 +1,12 @@
 program imprime_sim_ou_nao;
 var n : longint;
+
 {programa principal}
 begin
 	read(n);
-	if ((odd(n) and (n < -20)) or (not(odd(n)) and (n > 7))) then 
+	if ((odd(n) and (n < -20)) or (not(odd(n)) and (n > 7))) then 
 		write('SIM')
 	else 
 		write('NAO');
-end.
 
+end.
