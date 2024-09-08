25b282f2337394df7918562c8ebce739213a7ead
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-08 12:29:47 -0300

    Exercicio 111

--- Lista1A/Ex111.pas
+++ Lista1A/Ex111.pas
@@ -1,10 +1,13 @@
 program imprime_sim_ou_nao;
 var n : longint;
+
+{ programa principal }
 begin
 	read(n);
 	if ((n MOD 3 = 0) and (n MOD 7 = 0)) then
 		write('SIM')
 	else
 		write('NAO');
+
 end.
 
